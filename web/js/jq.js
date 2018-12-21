c7aadad6bca77f104280510c6d34f01b9c975717
fanghanquan <329650565@qq.com>
2018-12-21 22:49:43 +0800

    12.21first

--- web/js/jq.js
+++
@@ -1,4 +0,0 @@
-console.log(1)
-var a = document.getElementById('a')
-a.style="width:70px"
-console.log(a)
--- web/js/script.js
+++ web/js/script.js
@@ -1,1 +1,6 @@
-//lalalalalalala
+var para = document.getElementById("player1");
+para.addEventListener('click',updateName);
+function updateName() {
+    var name = prompt("输入一个新的名字");
+    para.textContent = "玩家1" + name;
+}
