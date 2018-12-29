de62f3c3c89906e17acac87d9635ba88e8c509ee
fanghanquan <329650565@qq.com>
2018-12-29 16:41:08 +0800

    12.29 1

--- picture/main.js
+++ picture/main.js
@@ -5,12 +5,15 @@ btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
 /* 遍历图片并添加至缩略图区 */
-for (var i = 1; i < 6; i++) {
+for (var i = 1; i <= 5; i++) {
   var newImage = document.createElement('img');
-  newImage.setAttribute('src',"images/pic" + i + ".jpg");
+  newImage.setAttribute('src','images/pic' + i + '.jpg');
   thumbBar.appendChild(newImage);
+  newImage.onclick = displayImage;
 }
-  
-
+function displayImage() {
+  var imgSrc = newImage.getAttribute('src');
+  displayedImage.setAttribute('src',imgSrc);
+}
 /* 编写 变亮/变暗 按钮 */
 
