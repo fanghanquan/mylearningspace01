bdc0e3a8f44de3ac36c1efffd37104b4e5d76e6f
fanghanquan <329650565@qq.com>
2018-12-29 21:00:43 +0800

    12.29 3

--- picture/main.js
+++ picture/main.js
@@ -9,11 +9,24 @@ for (var i = 1; i <= 5; i++) {
   var newImage = document.createElement('img');
   newImage.setAttribute('src','images/pic' + i + '.jpg');
   thumbBar.appendChild(newImage);
-  newImage.onclick = displayImage;
+  newImage.onclick = everyPic;
 }
-function displayImage() {
-  var imgSrc = newImage.getAttribute('src');
+function everyPic(e) {
+  var imgSrc = e.target.getAttribute('src');
   displayedImage.setAttribute('src',imgSrc);
 }
+
 /* 编写 变亮/变暗 按钮 */
 
+btn.onclick = function () {
+  var btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = '变亮';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  }else{
+    btn.setAttribute('class','dark');
+    btn.textContent = '变暗';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+}
