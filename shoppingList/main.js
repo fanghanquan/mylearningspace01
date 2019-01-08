a1e0488829d9054337bfaec44fda7a0c91b55892
fanghanquan <329650565@qq.com>
2019-01-08 21:57:30 +0800

    20190108 1

---
+++ shoppingList/main.js
@@ -0,0 +1,22 @@
+var newItem = document.querySelector('.newItem');
+var addItem = document.querySelector('.addItem');
+var shoppingList = document.querySelector('.shoppingList');
+addItem.onclick = function(){
+    var item = newItem.value;
+    if(!item){
+        alert('输入不能为空');
+        return false;
+    }
+    newItem.value = '';
+    var list = document.createElement('li');
+    shoppingList.appendChild(list);
+    list.textContent = item;
+    var button = document.createElement('button');
+    list.appendChild(button);
+    button.textContent = 'Delete';
+    button.setAttribute('class','delButton');
+    button.onclick = function(e){
+        shoppingList.removeChild(list);
+    }
+    newItem.focus();
+}
